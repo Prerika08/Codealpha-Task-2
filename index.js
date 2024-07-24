6679663dad68d3904a30442c829a7550b3f5ce34
Prerika08 <130048449+Prerika08@users.noreply.github.com>
2024-07-24 22:29:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("Hello");
+//window.alert("This is an alert!");
+
+//document.getElementById("thisish1").textContent = "Hello" ;
+//document.getElementById("thisisp").textContent = "Hello2" ;
+
+var username = window.prompt("What's you name?");
+document.write(username);
