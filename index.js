efa18678a024f0256afa33a945c09efc494ba25e
Prerika08 <130048449+Prerika08@users.noreply.github.com>
2024-08-22 23:22:57 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-//console.log("Hello");
-//window.alert("This is an alert!");
-
-//document.getElementById("thisish1").textContent = "Hello" ;
-//document.getElementById("thisisp").textContent = "Hello2" ;
-
-var username = window.prompt("What's you name?");
-document.write(username);
